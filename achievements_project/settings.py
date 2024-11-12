0fc6232631ee8251f41dab17f6459b4fffd1188e
narendra960 <103552794+narendra960@users.noreply.github.com>
2024-11-12 16:40:29 +0530

    Update settings.py

--- achievements_project/settings.py
+++ achievements_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^+53y@5u#9^*n^w1n9(%xvjbgrr!@f1qn=2$wq-oemq4a!&k6j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['achievement-task.onrender.com']
+ALLOWED_HOSTS = ['achievement-task.onrender.com','https://achievement-task.onrender.com']
 
 
 # Application definition
